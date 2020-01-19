77788be61bc2572caec4cef70e156aeba6f4392e
cybertek <github@cybertekschool.com>
2020-01-19 13:02:35 -0500

    day 3 selenium updated

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "com/vytrack/step_definitions/",
         dryRun = false,
-        tags = "@navigate or @login"
+        tags = "@login"
 )
 public class CukesRunner {
 }
--- src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -1,8 +1,14 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
 
 public class LoginStepDefs {
 
@@ -10,31 +16,48 @@ public class LoginStepDefs {
 
     @Given("the user is on the login page")
     public void the_user_is_on_the_login_page() throws InterruptedException {
-        System.out.println("I opened the browser and navigate to qa3.vytrack.com");
+        System.out.println("Opening the login page");
+        //Driver.get()--> this gets the webdriver
+        //driver ==Driver.get()
+        String url = ConfigurationReader.get("url");
+        Driver.get().get(url);
     }
 
     @When("the user enter the driver information")
     public void the_user_enter_the_driver_information() throws InterruptedException {
-
-        System.out.println("I entered user1 and UserUser123");
+        LoginPage loginPage = new LoginPage();
+        String username = ConfigurationReader.get("driver_username");
+        String password = ConfigurationReader.get("driver_password");
+        loginPage.login(username,password);
 
     }
 
     @Then("the user should be able to login")
     public void the_user_should_be_able_to_login() throws InterruptedException {
-        System.out.println("I verified that I see the dashboard page");
+
+        BrowserUtils.waitFor(2);
+        String actualTitle = Driver.get().getTitle();
+        Assert.assertEquals("Dashboard",actualTitle);
+
+
     }
 
     @When("the user enter the sales manager information")
     public void the_user_enter_the_sales_manager_information() {
-        System.out.println("I entered SalesManager123 and UserUser123");
+        LoginPage loginPage = new LoginPage();
+        String username = ConfigurationReader.get("sales_manager_username");
+        String password = ConfigurationReader.get("sales_manager_password");
+        loginPage.login(username,password);
 
 
     }
 
     @When("the user enter the store manager information")
     public void the_user_enter_the_store_manager_information() {
-        System.out.println("I entered StoreManager123 and UserUser123");
+        LoginPage loginPage = new LoginPage();
+        String username = ConfigurationReader.get("store_manager_username");
+        String password = ConfigurationReader.get("store_manager_password");
+        loginPage.login(username,password);
     }
 
 
