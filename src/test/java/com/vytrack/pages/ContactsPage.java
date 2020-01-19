65ea7a7c7d2498151913c53359c7d7283ccc3167
cybertek <github@cybertekschool.com>
2020-01-19 11:59:54 -0500

    day 3 selenium added

---
+++ src/test/java/com/vytrack/pages/ContactsPage.java
@@ -0,0 +1,18 @@
+package com.vytrack.pages;
+
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ContactsPage extends BasePage {
+
+
+    public WebElement getContactEmail(String email){
+        String xpath = "//*[contains(text(), '"+email+"') and @data-column-label='Email']";
+        return Driver.get().findElement(By.xpath(xpath));
+    }
+
+
+}
+
